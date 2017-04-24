5d6d373a330b1fa3faf4daec3e322bd1f8d2fb01
Rodrigo <rodrigoarreola96@gmail.com>
2017-04-24 14:26:14 -0500

    SQL

--- ptcsupport.sql
+++ ptcsupport.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-04-2017 a las 16:10:03
+-- Tiempo de generación: 24-04-2017 a las 21:25:38
 -- Versión del servidor: 10.0.17-MariaDB
 -- Versión de PHP: 5.5.30
 
@@ -46,18 +46,25 @@ CREATE TABLE `cursos` (
   `cuatrimestre` varchar(45) NOT NULL,
   `materia` varchar(45) NOT NULL,
   `horario` varchar(45) NOT NULL,
-  `grupo` varchar(45) NOT NULL
+  `grupo` varchar(45) NOT NULL,
+  `politicas` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Volcado de datos para la tabla `cursos`
 --
 
-INSERT INTO `cursos` (`id`, `idptc`, `cuatrimestre`, `materia`, `horario`, `grupo`) VALUES
-(1, 8, '8', 'IA', '10-12', 'A'),
-(2, 8, '8', 'Mantenimiento', '08-10', 'A'),
-(3, 8, '8', 'Planeacion', '12-16', 'A'),
-(4, 8, '8', 'Móviles', '09-11', 'A');
+INSERT INTO `cursos` (`id`, `idptc`, `cuatrimestre`, `materia`, `horario`, `grupo`, `politicas`) VALUES
+(1, 8, '8', 'IA', '10-12', 'A', ''),
+(2, 8, '8', 'Mantenimiento', '08-10', 'A', ''),
+(3, 8, '8', 'Planeacion', '12-16', 'A', ''),
+(4, 8, '8', 'Móviles', '09-11', 'A', ''),
+(5, 11, '5', 'Redes', '10-12', 'A', ''),
+(6, 11, '5', 'Algebra', '08- 12', 'B', ''),
+(7, 8, '2', 'as', '09-11', 'sad', ''),
+(8, 8, '1', 'Perro', '16-18', 'Z', ''),
+(9, 8, '1', 'MatemÃ¡ticas', '18-20', 'Q', 'No comer\r\nNo hablar \r\n'),
+(10, 8, '8', 'Rein', '10-12', 'A', 'fgh');
 
 -- --------------------------------------------------------
 
@@ -81,15 +88,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `matricula`, `cuatrimestre`, `email`, `tipo`, `contrasena`) VALUES
-(1, 'q', 'q', '1', '1', 'q@s', 'alumno', 'q'),
-(2, 'Rodrigo', 'Arreola', '143415', '8', 'rodrigo@demo.com', 'alumno', 'demo'),
-(3, 'Victor', 'Fon Bon', '1111', NULL, 'fonbon@yopmail.com', 'ptc', 'demo'),
-(4, 'Aremy', 'Virrueta', '2222', '', 'aremy@demo', 'alumno', 'qwer'),
-(5, 'demo', 'demo', '1234', '', 'demo@demo', 'alumno', 'demo'),
-(6, 'Ali', 'Zunun', '1234', '', 'qwer@qwer', 'alumno', '1234'),
 (8, 'Argelia', 'Najera', '0987', NULL, 'asd@asd', 'PTC', '0987'),
 (9, 'Daniel', 'Nandayapa', '123', '8', 'asd@asd', 'Alumno', 'zxc'),
-(10, 'Fernando', 'Herrera', '098765', '8', 'asd@asd', 'Alumno', 'asd');
+(10, 'Fernando', 'Herrera', '098765', '8', 'asd@asd', 'Alumno', 'asd'),
+(11, 'Miguel', 'Kioshy', '9090', NULL, 'nakamura@asd.com', 'PTC', 'kiokio'),
+(12, 'Rodrigo', 'Arreola', '143415', '8', 'rodrigo@yopmail.com', 'Alumno', 'demo');
 
 --
 -- Índices para tablas volcadas
@@ -128,12 +131,12 @@ ALTER TABLE `alumnocursos`
 -- AUTO_INCREMENT de la tabla `cursos`
 --
 ALTER TABLE `cursos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
